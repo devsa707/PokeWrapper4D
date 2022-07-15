e03a12e2fa1b96c6fb28ac2beb730cc7df214821
devsa707 <101226180+devsa707@users.noreply.github.com>
2022-07-14 22:45:34 -0300

    Add PokeAPI.Types

---
+++ Source/PokeAPI.Types.pas
@@ -0,0 +1,33 @@
+unit PokeAPI.Types;
+
+interface
+
+type
+  TBerries = (berry, //
+    berry_firmness, //
+    berry_flavor);
+
+  TContests = (contest_types, //
+    contest_effects, //
+    super_contest_effects);
+
+  TEncounters = (encounter_methods, //
+    enconter_conditions, //
+    encounter_condition_values);
+
+  TEvolution = (evolution_chains, //
+    evolution_triggers);
+
+  TGames = (generations, //
+    pokedexes, //
+    version, //
+    version_groups);
+
+  TItems = (item, //
+    item_attributes, //
+    item_fling_effects, //
+    item_pockets);
+
+implementation
+
+end.
