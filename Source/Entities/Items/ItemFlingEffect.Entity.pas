3bcfb9e6feec3ea649ee340601dc2e0e6b1a2b6e
devsa707 <101226180+devsa707@users.noreply.github.com>
2022-07-25 20:19:19 -0300

    Create ItemFlingEffect.Entity.pas

---
+++ Source/Entities/Items/ItemFlingEffect.Entity.pas
@@ -0,0 +1,100 @@
+unit ItemFlingEffect.Entity;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Generics.Collections;
+
+type
+  TItemFlingEffectEntity = class
+  type
+    TEffectEntries = class
+    type
+      TLanguage = class
+      private
+        Fname: string;
+        Furl: string;
+      public
+        property name: string read Fname write Fname;
+        property url: string read Furl write Furl;
+      end;
+    private
+      Feffect: string;
+      Flanguage: TLanguage;
+    public
+      constructor Create; overload;
+      destructor Destroy; override;
+      property effect: string read Feffect write Feffect;
+      property language: TLanguage read Flanguage write Flanguage;
+    end;
+
+    TItems = class
+    private
+      Fname: string;
+      Furl: string;
+    public
+      property name: string read Fname write Fname;
+      property url: string read Furl write Furl;
+    end;
+  private
+    Feffect_entries: TObjectList<TEffectEntries>;
+    Fid: integer;
+    Fitems: TObjectList<TItems>;
+    Fname: string;
+    procedure SetFeffect_entries(const Value: TObjectList<TEffectEntries>);
+    procedure SetFitems(const Value: TObjectList<TItems>);
+  public
+    constructor Create; overload;
+    destructor Destroy; override;
+    property effect_entries: TObjectList<TEffectEntries> read Feffect_entries
+      write SetFeffect_entries;
+    property id: integer read Fid write Fid;
+    property items: TObjectList<TItems> read Fitems write SetFitems;
+    property name: string read Fname write Fname;
+  end;
+
+implementation
+
+{ TItemFlingEffectEntity.TEffectEntries }
+
+constructor TItemFlingEffectEntity.TEffectEntries.Create;
+begin
+  Flanguage := TLanguage.Create;
+end;
+
+destructor TItemFlingEffectEntity.TEffectEntries.Destroy;
+begin
+  Flanguage.Free;
+  inherited;
+end;
+
+{ TItemFlingEffectEntity }
+
+constructor TItemFlingEffectEntity.Create;
+begin
+  Feffect_entries := TObjectList<TEffectEntries>.Create;
+  Fitems := TObjectList<TItems>.Create;
+end;
+
+destructor TItemFlingEffectEntity.Destroy;
+begin
+  Feffect_entries.Free;
+  Fitems.Free;
+  inherited;
+end;
+
+procedure TItemFlingEffectEntity.SetFeffect_entries
+  (const Value: TObjectList<TEffectEntries>);
+begin
+  FreeAndNil(Feffect_entries);
+  Feffect_entries := Value;
+end;
+
+procedure TItemFlingEffectEntity.SetFitems(const Value: TObjectList<TItems>);
+begin
+  FreeAndNil(Fitems);
+  Fitems := Value;
+end;
+
+end.
